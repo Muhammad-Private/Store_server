8b8ee48a6d45a51110a7bbb181a9d69edd192f4a
muhammad <allappsdata97@gmail.com>
2024-07-13 10:58:18 +0300

    fc

--- controllers/products_controller.js
+++ controllers/products_controller.js
@@ -34,7 +34,7 @@ async function AddProduct(req, res)
   catch (error) 
   {
     console.log(error);
-    res.status(500).json({ message: error.message });
+    res.status(500).json({ message: error });
   }
 }
 
